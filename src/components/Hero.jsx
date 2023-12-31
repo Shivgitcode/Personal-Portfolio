f2923503953c29458af899f978933b5880624b7b
shivansh <shivneeraj2004@gmail.com>
2023-12-31 22:35:25 +0530

    making my portfolio responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,41 +5,42 @@ import Typed from "react-typed";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col w-full justify-evenly items-center relative top-28 px-[16px] ">
-      <div className="absolute -top-16">
+    <section className="flex flex-col w-full justify-evenly items-center relative top-28 px-[16px] lg:flex-row-reverse lg:item lg:w-[60%] mx-auto lg:justify-between lg:px-[0px] lg:my-[35px] ">
+      <div className="absolute -top-16 lg:-right-24">
         <HeroBgAnimation></HeroBgAnimation>
       </div>
-      <div className="w-[300px] h-[300px] mb-[30px]">
+      <div className="w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] mb-[30px] lg:flex-1 lg:z-10">
         <img
           src={myimage}
           alt=""
-          className="w-full h-full rounded-full object-cover border-2 border-fuchsia-500"
+          className="w-full h-full lg:max-w-[400px] lg:max-h-[400px] lg:ml-auto rounded-full object-cover border-2 border-fuchsia-500"
         />
       </div>
-
-      <h1 className="text-[#f2f3f4] text-[40px] leading-[48px] font-bold font-poppins text-center mb-[8px] max-w-[413px]">
-        Hi, I am Shivansh Aggarwal
-      </h1>
-      <p className="text-[22px] font-semibold font-poppins leading-[48px] text-center text-[#f2f3f4] mb-[16px]">
-        I am a{" "}
-        <span className="text-[#854ce6]">
-          <Typed
-            strings={["Web Developer"]}
-            typeSpeed={130}
-            backSpeed={50}
-            loop
-          ></Typed>
-        </span>
-      </p>
-      <p className="text-center text-[16px] leading-[32px] text-[#f2f3f4]/60 font-normal font-poppins mb-[42px] ">
-        I am a motivated and versatile individual, always eager to take on new
-        challenges. With a passion for learning I am dedicated to delivering
-        high-quality results. With a positive attitude and a growth mindset, I
-        am ready to make a meaningful contribution and achieve great things.
-      </p>
-      <button className="font-poppins text-white font-medium text-[16px] text-center leading-[32px] rounded-[20px] py-[12px] bg-gradient-to-r from-fuchsia-500 to-purple-500 w-full max-w-[400px] hover:scale-105 transition-all ">
-        Check Resume
-      </button>
+      <div className="flex flex-col items-center lg:items-start flex-1  ">
+        <h1 className="text-[#f2f3f4] text-[40px] lg:text-[50px] lg:text-start leading-[48px] font-bold font-poppins text-center mb-[8px] max-w-[413px]">
+          Hi, I am Shivansh Aggarwal
+        </h1>
+        <p className="text-[22px] lg:text-[32px] lg:leading-[68px]  font-semibold font-poppins leading-[48px] text-center text-[#f2f3f4] mb-[16px] lg:text-start">
+          I am a{" "}
+          <span className="text-[#854ce6]">
+            <Typed
+              strings={["Web Developer"]}
+              typeSpeed={130}
+              backSpeed={50}
+              loop
+            ></Typed>
+          </span>
+        </p>
+        <p className="text-center lg:text-start text-[16px] lg:text-[20px] leading-[32px] text-[#f2f3f4]/60 font-normal font-poppins mb-[42px] ">
+          I am a motivated and versatile individual, always eager to take on new
+          challenges. With a passion for learning I am dedicated to delivering
+          high-quality results. With a positive attitude and a growth mindset, I
+          am ready to make a meaningful contribution and achieve great things.
+        </p>
+        <button className="font-poppins text-white font-medium text-[16px] text-center leading-[32px] rounded-[20px] py-[12px] bg-gradient-to-r from-fuchsia-500 to-purple-500 w-full max-w-[400px] hover:scale-105 transition-all ">
+          Check Resume
+        </button>
+      </div>
     </section>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,15 +10,17 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="flex flex-row w-[85%] justify-between items-center mx-auto relative py-3">
-      <h1 className="text-white font-bold font-poppins text-xl">Shivansh</h1>
-      <button onClick={handleClick}>
+    <nav className="flex flex-row w-[85%] xl:w-[60%] justify-between items-center mx-auto relative py-3">
+      <h1 className="text-white font-bold font-poppins text-xl xl:flex-1">
+        Shivansh
+      </h1>
+      <button onClick={handleClick} className="xl:hidden">
         <FaBars fill="white" fontSize="20px" />
       </button>
       <ul
-        className={`absolute flex flex-col items-start top-12 w-screen -left-12 bg-[#191924]/60 rounded-b-2xl px-[40px] pt-[12px] pb-[24px] ${
+        className={`absolute flex flex-col items-start top-12 w-screen -left-12 bg-[#191924]/60 rounded-b-2xl px-[40px] pt-[12px] pb-[24px] pl-[49px] ${
           show ? "opacity-0" : "opacity-100"
-        } transition-all shadow-lg`}
+        } transition-all shadow-lg  xl:hidden`}
       >
         {navlinks.map((el, indx) => {
           return (
@@ -34,6 +36,26 @@ export default function Navbar() {
           Github Profile
         </button>
       </ul>
+
+      <ul
+        className={`hidden lg:flex flex-row justify-between items-center flex-1 gap-x-[32px] max-h-fit`}
+      >
+        {navlinks.map((el, indx) => {
+          return (
+            <li
+              key={indx}
+              className="text-[16px] font-poppins font-semibold text-[#f2f3f4] cursor-pointer hover:text-[#854ce6] transition-all"
+            >
+              {el}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="hidden xl:flex flex-1">
+        <button className="w-full max-w-[149px] px-[16px] py-[10px] font-poppins text-[14px] border-[1px] border-[#854ce6] text-[#854ce6] rounded-[20px] ml-auto hover:text-white hover:bg-[#854ce6] transition-all duration-200">
+          Github Profile
+        </button>
+      </div>
     </nav>
   );
 }
